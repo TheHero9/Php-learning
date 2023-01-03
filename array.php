dcf3114a76f5d7642ad5671c33a9bcbc681071db
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2023-01-03 18:48:38 +0200

    Array methods

---
+++ array.php
@@ -0,0 +1,77 @@
+<?php
+    $numbers = [ 44, 20, 29, 1];
+    $fruits = ['apple', 'orange', 'pear'];
+
+    // print_r($numbers);
+    // var_dump($numbers);
+
+    // echo $numbers[1]
+
+    // $people = [
+    //     [
+    //         "name" => "Dimi",
+    //         "number" => "9"
+    //     ],
+    //     [
+    //         "name" => "Kala",
+    //         "number" => "10"
+    //     ]
+    //     ];
+
+    // echo $people[1]["number"];
+
+
+
+    //Length
+    // echo count($numbers);
+    // Search array
+    // var_dump(in_array("1", $numbers));
+
+
+    // Add to array
+    // $fruits[] = 'grape';
+    // array_push($fruits, 'mango', "pineapple");
+
+    // array_unshift($fruits, 'kiwi');
+    // print_r($fruits);
+
+
+    // Remove from array
+    // array_pop($fruits);
+    // array_shift($fruits);
+    // print_r($fruits);
+    
+
+    //Split into chunks
+    // $chunkedArray = array_chunk($fruits, 3);
+    // print_r($chunkedArray);
+
+
+    //Merge
+    // $mergedArr = array_merge($numbers, $fruits);
+    // $mergedArr2 = [...$numbers, ...$fruits];
+    // print_r($mergedArr2)
+    
+
+    // Combine
+    // $a = ["green", "red"];
+    // $b = ["kiwi", "apple"];
+
+    // $c = array_combine($a, $b);
+    // print_r($c);
+
+    // $keys = array_keys($c);
+    // print_r($keys);
+
+    // $flipped = array_flip($c);
+    // print_r($flipped)
+
+    //Range
+    // $to20 = range(1, 20);
+    // print_r($to20);
+    
+
+    //Filter
+    // $less10 = array_filter($numbers, fn($el)=> $el<10);
+    // print_r($less10);
+    ?>
